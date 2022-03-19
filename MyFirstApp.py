62e63a00a5a1c34ba1bb9a5a772e7bd18d047efa
sabrizin <98398474+sabrizin@users.noreply.github.com>
2022-03-19 12:14:12 +0800

    Update MyFirstApp.py

--- MyFirstApp.py
+++ MyFirstApp.py
@@ -8,3 +8,10 @@ st.write(pd.DataFrame({
     'Intplan': ['yes', 'yes', 'yes', 'no'],
     'Churn Status': [0, 0, 0, 1]
 }))
+
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
